3b61dc44e8510901e7ae7da0f3e6ae80dff4fbfe
Pradeep <paddum19@gmail.com>
2021-06-07 11:22:41 +0530

    Contact message edited,Log Dependecny,Context path added

--- PhysicalTrainerV1/src/main/java/com/mp/PhysicalTrainerV1/controllers/ContactController.java
+++ PhysicalTrainerV1/src/main/java/com/mp/PhysicalTrainerV1/controllers/ContactController.java
@@ -1,6 +1,11 @@
 package com.mp.PhysicalTrainerV1.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.ext.XLogger;
+import org.slf4j.ext.XLoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,25 +22,29 @@ import com.mp.common.codes.MessageCode;
 @RequestMapping("contact")
 public class ContactController {
 
+	private XLogger logger = XLoggerFactory.getXLogger(ContactController.class.getName());
+
+	private static final String BEGIN_REQUEST_LOG = "%s request";
+
 	@Autowired
 	private ContactService contactService;
-	
-	
+
+
 	@Autowired
 	public ContactController(ContactService theContactService) {
 		contactService=theContactService;
 	}
-	
-	
-	
-	
-	@PostMapping()
-	public ContactResponse createContact(@RequestBody ContactRequest request) {
-		
-		
+
+
+	@PostMapping
+	public ContactResponse createContact(@RequestBody ContactRequest request,HttpServletRequest httpRequest) {
+		logger.entry(String.format(BEGIN_REQUEST_LOG, httpRequest.getRequestURI()));
+
 		ContactEntity entity=ContactMapper.fromContactRequest(request);
 		ContactEntity created=contactService.saveContact(entity);
-		
+
+		logger.exit(String.format("locationId=%s", created.getContactId()));
 		return ContactMapper.toContactResponse(created, MessageCode.CREATE_CONTACT_SUCCESS);
 	}
+	
 }
